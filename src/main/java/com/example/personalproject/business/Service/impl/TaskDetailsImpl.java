511c8dfdda4e71ea35a496adb56fd6ed6538a32d
flackx <brendons.liflands@va.lv>
2023-06-20 11:11:49 +0300

    TaskDetails Post method created

--- src/main/java/com/example/personalproject/business/Service/impl/TaskDetailsImpl.java
+++ src/main/java/com/example/personalproject/business/Service/impl/TaskDetailsImpl.java
@@ -1,24 +1,32 @@
 package com.example.personalproject.business.Service.impl;
 
 import com.example.personalproject.business.Repository.TaskDetailsRepository;
+import com.example.personalproject.business.Repository.TaskRepository;
 import com.example.personalproject.business.Service.TaskDetailsService;
+import com.example.personalproject.model.Task;
 import com.example.personalproject.model.TaskDetails;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TaskDetailsImpl implements TaskDetailsService {
 
     private final TaskDetailsRepository taskDetailsRepository;
-
+    private final TaskRepository taskRepository;
     @Autowired
-    public TaskDetailsImpl(TaskDetailsRepository taskDetailsRepository) {
+    public TaskDetailsImpl(TaskDetailsRepository taskDetailsRepository, TaskRepository taskRepository) {
         this.taskDetailsRepository = taskDetailsRepository;
+        this.taskRepository = taskRepository;
     }
-
-
-    @Override
+    @Transactional
     public TaskDetails createTaskDetails(Long taskId, TaskDetails taskDetails) {
-        return null;
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new EntityNotFoundException("Task not found with id: " + taskId));
+
+        taskDetails.setTask(task);
+        TaskDetails createdTaskDetails = taskDetailsRepository.save(taskDetails);
+        return createdTaskDetails;
     }
 }
